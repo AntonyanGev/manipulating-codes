7b2da47195e3829646b996f68955cc0aba1fc17d
AntonyanGev <138504476+AntonyanGev@users.noreply.github.com>
2024-03-03 00:31:56 +0400

    Update codes.js

--- codes.js
+++ codes.js
@@ -31,6 +31,81 @@ function countAndPrint(num) {
     multiply: multiply,
   };
 }
+///second solution
+
+function countAndPrint(num) {
+  if (typeof num !== "number") {
+    console.log(" Dear user please input a number");
+    return;
+  }
+  let res = num.toString();
+  let result = res.split("");
+  let count = 0;
+  let sum = 0;
+  let multiply = 1;
+  let reversed = [];
+  let normal = [];
+  for (let i = 0; i < result.length; i++) {
+      if(!isNaN(result[i])){
+    count++;
+    sum = sum + parseInt(result[i]);
+    multiply *= parseInt(result[i]);
+    normal.push(result[i]);
+    reversed.push(result.at(-1 - i));
+  }
+  }
+
+  normal = normal.toString();
+  reversed = reversed.toString();
+  console.log(normal);
+  console.log(reversed);
+
+  return {
+    numOfElements: count,
+    sum: sum,
+    multiply: multiply,
+  };
+}
+
+
+
+
+
+///3-d solution is the most optimal 
+function countAndPrint(num) {
+  if (typeof num !== "number") {
+    console.log(" Dear user please input a number");
+    return;
+  }
+    let integer=Math.floor(num)
+  let res =integer.toString();
+  let result = res.split("");
+  let count = 0;
+  let sum = 0;
+  let multiply = 1;
+  let reversed = [];
+  let normal = [];
+  for (let i = 0; i < result.length; i++) {
+      if(!isNaN(result[i])){
+    count++;
+    sum = sum + parseInt(result[i]);
+    multiply *= parseInt(result[i]);
+    normal.push(result[i]);
+    reversed.push(result.at(-1 - i));
+  }
+  }
+
+  normal = normal.toString();
+  reversed = reversed.toString();
+  console.log(normal);
+  console.log(reversed);
+
+  return {
+    numOfElements: count,
+    sum: sum,
+    multiply: multiply,
+  };
+}
 
 
 ///226
